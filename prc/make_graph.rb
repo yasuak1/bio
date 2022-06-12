c9b850765c62e11392b5f9bc3faecde832cbb144
yasuak1 <yasuakimizuno36@gmail.com>
2022-06-12 17:02:06 +0900

    Add information of weight

--- prc/make_graph.rb
+++ prc/make_graph.rb
@@ -3,6 +3,7 @@ require 'bio'
 Bio::FlatFile.auto(ARGF) do |ff|
     dpoint = Hash.new(0)
     ff.each_entry do |ent|
+        # define amino vector
         arg = 0
         darg = 360 / ent.seq.chars.uniq.size
         ent.seq.chars.uniq.sort.each do |amino|
@@ -12,18 +13,40 @@ Bio::FlatFile.auto(ARGF) do |ff|
             arg += darg
         end
 
+        # define amino weight
+        amino_weight = Hash.new(0)
+        lseq = ent.seq.length
+        ent.seq.composition.each do |key, val|
+            amino_weight[key] = val.to_f / lseq
+        end
+
+        # plot data
+        INF = (1 << 21)
+        x_range = [INF, -INF]
+        y_range = [INF, -INF]
+        multiply_wight = true
         points = Array.new
         points << [0, 0]
         ent.seq.chars.each do |amino|
             point = points[-1].dup
             point.each_with_index do |val ,index|
-                point[index] += dpoint[amino][index] 
+                point[index] += multiply_wight ? dpoint[amino][index] * amino_weight[amino] : dpoint[amino][index] 
+                x_range[0] = [x_range[0], point[0]].min
+                x_range[-1] = [x_range[-1], point[0]].max
+                y_range[0] = [y_range[0], point[-1]].min
+                y_range[-1] = [y_range[-1], point[-1]].max
             end
             points << point
         end
         points.each do |point|
             puts "#{point[0]} #{point[-1]}"
         end
+
+        puts "# set range"
+        x_range.map!{|val| (val * 1.1).round(2)}
+        y_range.map!{|val| (val * 1.1).round(2)}
+        puts "# x [#{x_range[0]}, #{x_range[-1]}]"
+        puts "# y [#{y_range[0]}, #{y_range[-1]}]"
     end
 =begin
     amino_vec.each do |key, val|
