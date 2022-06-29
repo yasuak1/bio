d0b5981bf7aad39cde5094570d3753667569160e
yasuak1 <yasuakimizuno36@gmail.com>
2022-06-29 12:33:03 +0900

    Modify

--- data/make_npy.py
+++ data/make_npy.py
@@ -5,8 +5,8 @@ import cv2
 
 def get_data(path):
     dict = {}
-    #with open(path + 'train.pkl', 'rb') as tf:
-    with open(path + 'test.pkl', 'rb') as tf:
+    with open(path + 'train.pkl', 'rb') as tf:
+    #with open(path + 'test.pkl', 'rb') as tf:
         dict = pickle.load(tf)
     return dict
 
@@ -16,12 +16,14 @@ def main():
     cnt = 0
     img_data = np.empty(0)
     y_train = np.empty(0)
-    path = '/home/yasu/bio/data/'
-    dict = get_data(path)
+    #path = '/home/yasu/bio/data/'
+    #dict = get_data(path)
+    dict = get_data('')
     for key in dict:
         if cnt == 10: break
         #print(key + ' ' + dict[key])
-        img_path = path + key + '.png'
+        #img_path = path + key + '.png'
+        img_path = 'train/' + key + '.png'
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         #print(img.shape)
@@ -31,10 +33,12 @@ def main():
     img_data = img_data.reshape(cnt, 1, 480, 640)
     print(img_data.shape)
     #np.save('train', img_data)
-    data['x_test'] = img_data
-    data['y_test'] = y_train
-    np.save('test', data)
-
+    data['x_train'] = img_data
+    data['y_train'] = y_train
+    #np.save('train', data)
+    #np.save('test', data)
+    with open("train_data.pkl", "wb") as tf:
+        pickle.dump(data, tf)
 
 if __name__ == '__main__':
     main()
--- data/train_fnn.py
+++ data/train_fnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 if __name__ == '__main__':
-    data = np.load('test.npy', allow_pickle=True)
-    print(data['x_test'])
+    data = np.load('train_data.pkl', allow_pickle=True)
+    for key in data:
+        print(data[key].shape)
